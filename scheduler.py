0809cdb7905678595303433cb6f8146cfd29d9e5
Michael Song <mjysong@gmail.com>
2020-05-05 08:43:38 -0400

    update every 20 min

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,19 @@
 import aws
 from apscheduler.schedulers.background import BlockingScheduler
 import utils
+from datetime import datetime
 
 def update_data():
-    aws.update_tables()
-    utils.send_email()
-    print('email sent')
+    now = datetime.now()
+    if now.hour == 6 and now.minute < 20:
+        aws.update_tables()
+        utils.send_email()
+        print('email sent')
 
 # Schedule a database update every day at 3am.
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=8)
+@sched.scheduled_job('cron', minute = '1, 21, 41')
 def scheduled_job():
     update_data()
 
